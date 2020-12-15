9e4ab9c47d6d41e1de0b5127df574d8cbd989784
Stefan Radomski <stefan.radomski@ionos.com>
2020-12-15 11:01:57 +0100

    meh

--- dim-web/setup.py
+++ dim-web/setup.py
@@ -5,9 +5,6 @@ setup(name='cas',
       packages=['cas'],
       data_files=[
           ('share/dim-web', ['www/index.html']),
-          ('share/dim-web/css', ['www/css']),
-          ('share/dim-web/js', ['www/js']),
-          ('share/dim-web/images', ['www/images']),
-          ('share/dim-web/font-awesome-4.7.0', ['www/font-awesome-4.7.0']),
+          ('share/dim-web/static', ['www/css']),
           ],
       version=version.VERSION)
