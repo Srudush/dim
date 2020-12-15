607ee1e704491aafbf20fb88b501937e3544c613
Stefan Radomski <stefan.radomski@ionos.com>
2020-12-15 10:52:54 +0100

    meh

--- dim-web/setup.py
+++ dim-web/setup.py
@@ -4,10 +4,10 @@ from cas import version
 setup(name='cas',
       packages=['cas'],
       data_files=[
-          {'share/dim-web', ['www/index.html']},
-          {'share/dim-web/css', ['www/css']},
-          {'share/dim-web/js', ['www/js']},
-          {'share/dim-web/images', ['www/images']},
-          {'share/dim-web/font-awesome-4.7.0', ['www/font-awesome-4.7.0']},
+          ('share/dim-web', ['www/index.html']),
+          ('share/dim-web/css', ['www/css']),
+          ('share/dim-web/js', ['www/js']),
+          ('share/dim-web/images', ['www/images']),
+          ('share/dim-web/font-awesome-4.7.0', ['www/font-awesome-4.7.0']),
           ],
       version=version.VERSION)
