b87e400d1b41135578cfcee11f54a1772499f598
Stefan Radomski <stefan.radomski@ionos.com>
2020-12-15 12:33:29 +0100

    meh

--- dim-web/setup.py
+++ dim-web/setup.py
@@ -5,6 +5,6 @@ from cas import version
 setup(name='cas',
       packages=['cas'],
       data_files=[
-          ('share/dim-web', glob.glob('www/**', recursive=True)),
+          ('share/dim-web', glob.glob('www/**.*', recursive=True)),
           ],
       version=version.VERSION)
