94fdc0386bbccbe4c44df7d8b8a6c82b5c3b43da
Stefan Radomski <stefan.radomski@ionos.com>
2020-12-15 11:06:58 +0100

    meh

--- dim-web/setup.py
+++ dim-web/setup.py
@@ -5,6 +5,6 @@ setup(name='cas',
       packages=['cas'],
       data_files=[
           ('share/dim-web', ['www/index.html']),
-          ('share/dim-web/static', ['www/css']),
+          ('share/dim-web/static', ['www/static']),
           ],
       version=version.VERSION)
